51e20557db1a754f7aafeef2e3f4c9ee494583aa
Maxence d'Espeuilles <maxence@macbookpro.home>
2011-03-25 17:19:40 +0100

    Ajout de la licence dans le fichier Shared/main.m

--- Shared/main.m
+++ Shared/main.m
@@ -1,10 +1,14 @@
-//
-//  main.m
-//  iOpquast
-//
-//  Created by Maxence d'Espeuilles on 19/09/10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
-//
+/**
+ * iOpquast
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to contact@openaccess.fr so we can send you a copy immediately.
+ */
 
 #import <UIKit/UIKit.h>
 
